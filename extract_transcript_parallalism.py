5500abb5e9ae79bc3623e09db0ad052511387241
Siwon-Kim <siwonkim1108@gmail.com>
2023-09-13 15:53:44 -0400

    extract sample 100 dataset

--- extract_transcript_parallalism.py
+++ extract_transcript_parallalism.py
@@ -22,18 +22,26 @@ def process_transcript_extraction(video_id):
     return text
 
 # contain video ids in a list
+count = 0
 video_ids = []
-f = open('video_ids.txt', 'r')
+f = open('news_video_ids.txt', 'r')
 for id in f:
     video_id = id.replace("\n", "")
     video_ids.append(video_id)
 
+    count += 1
+    if count >= 100:
+        break 
+
+print(video_ids)
 wb = Workbook()
 sheet1 = wb.add_sheet('Sheet 1')
 
 # Write the column headers
 sheet1.write(0, 0, 'Video ID')
 sheet1.write(0, 1, 'Transcript')
+
+MAX_CELL_LENGTH = 32767
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     futures = {executor.submit(process_transcript_extraction, video_id): video_id for video_id in video_ids}
@@ -45,13 +53,18 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
         video_id = futures[future]
         try:
             transcript = future.result()
+
+            if len(transcript) > MAX_CELL_LENGTH:
+                # Truncate the transcript to the maximum allowed length
+                transcript = transcript[:MAX_CELL_LENGTH]
+
             sheet1.write(row, 0, video_id)
             sheet1.write(row, 1, transcript)
             row += 1
         except Exception as e:
             print(f"An error occurred for video ID {video_id}: {str(e)}")
 
-wb.save('comment_writer_dataset_w_transcript.xls')
+wb.save('news_dataset_w_transcript.xls')
 
 end_time = time.time()
 
