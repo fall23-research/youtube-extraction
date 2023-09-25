295a1b68196c12fc47851682d5e22fe206af76bb
Siwon-Kim <siwonkim1108@gmail.com>
2023-09-25 17:53:18 -0400

    feat: filter only official news channel ids

--- 댓글작성자/extract_only_news_id.py
+++ 댓글작성자/extract_only_news_id.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_category(video_id):
+def get_channelId(video_id):
     API_KEY = os.getenv('API_KEY')
 
     # URL for the GET request
@@ -18,14 +18,16 @@ def get_category(video_id):
         try:
         # Parse the JSON response
             video_metadata = response.json()
-            print(video_id, ": YouTube Category =", video_metadata['items'][0]['snippet']['categoryId']) # 25 is under News & Politics
-            return video_metadata['items'][0]['snippet']['categoryId']
+            channelId = video_metadata['items'][0]['snippet']['channelId']
+            print(video_id, ": YouTube Channel ID =", channelId)
+            return channelId
         except Exception as e:
-            print("Category Unavailable") 
+            print("News Channel Unavailable") 
         
     else:
         print(f"Error: {response.status_code} - {response.text}")
 
+official_news_channel_ids = ['UChlgI3UHCOnwUGzWzbJ3H5w', 'UCkinYTS9IHqOEwR1Sze2JTw', 'UCF4Wxdo3inmxP-Y59wXDsFw', 'UCsU-I-vHLiaMfV_ceaYz5rQ', 'UCcQTRi69dsVYHN3exePtZ1A', 'UCfq4V1DAuaojnr2ryvWNysw', 'UCG9aFJTZ-lMCHAiO1KJsirg', 'UCWlV3Lz_55UaX4JsMj-z__Q', 'UCTHCOPwqNfZ0uiKOvFyhGwg']
 
 # append video_id only with categoryId == 25
 wf = open('commenter_news_video_ids.txt', 'a') 
@@ -36,8 +38,8 @@ with open('commenter_video_id.txt', 'r') as rf:
 
 for id in lines[0:10000]:
     video_id = id.replace("\n", "")
-    if get_category(video_id) != '25':
-        continue
-    else:
+    if get_channelId(video_id) in official_news_channel_ids:
         wf.write(video_id)
         wf.write('\n')
+    else:
+        continue
